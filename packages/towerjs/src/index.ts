ae7a173c90dcaf0933cc913f7f9f64ecda2185ef
Jasper Gorchov <jaspergorchov@icloud.com>
2026-07-23 14:26:32 -0700

    Add towerjs public package

---
+++ packages/towerjs/src/index.ts
@@ -0,0 +1,11 @@
+export { defineTower } from "@towerjs/blueprint";
+export type { ServiceRegistry, TowerInitContext, TowerModule, TowerBlueprint } from "@towerjs/blueprint";
+
+export { createTowerApp } from "@towerjs/foundation";
+export type { TowerApp, TowerRuntime, TowerConfig } from "@towerjs/foundation";
+
+export { vault } from "@towerjs/vault";
+export type { VaultDb, VaultModule } from "@towerjs/vault";
+
+export { gatehouse } from "@towerjs/gatehouse";
+export type { GatehouseModule, TowerUser } from "@towerjs/gatehouse";
